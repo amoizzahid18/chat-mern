cb08c604f89d0200fe87764a8e751acdbc2da37a
Moiz <a.moiz.zahid.18@gmail.com>
2025-08-03 16:11:26 +0500

    create server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
