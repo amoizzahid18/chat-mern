9511d3fc4e596ff8298340e81ccc6ffc28223610
Moiz <a.moiz.zahid.18@gmail.com>
2025-08-07 00:00:31 +0500

    improve form's ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <div className='bg-gradient-to-b from-purple-700 to-purple-400 h-screen w-full flex justify-center items-center' >
       <Login />
+      {/* <Signup /> */}
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -50,7 +50,7 @@ function Login() {
   );
   return (
     <>
-      <div className=" bg-base-200 border-base-300 rounded-box w-lg border p-7 py-12">
+      <div className=" bg-base-200 border-base-300 rounded-box w-lg border p-7 pt-10">
         <h1 className="text-3xl font-bold text-center mb-8">Login</h1>
 
         <div className="my-4">
@@ -83,12 +83,9 @@ function Login() {
               title="Only letters, numbers or dash"
             />
           </label>
-          <p className="validator-hint hidden">
-            Must be 3 to 30 characters
-          </p>
+          <p className="validator-hint hidden">Must be 3 to 30 characters</p>
         </div>
 
-       
         <div className="my-4">
           <label className="label block">Password</label>
           <label className="input validator w-full">
@@ -150,12 +147,46 @@ function Login() {
         </div>
 
         <button className="btn btn-neutral w-full  mt-4">Login</button>
-
-        <div>
-            Don't have an account?
-            <span>Sign Up Now</span>
+        <div className="text-center mt-4 px-2">Or Sign In using</div>
+        <div className="flex justify-center rounded-full mx-24 py-6 btn btn-neutral items-center my-8 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="30"
+            height="30"
+            viewBox="0,0,256,256"
+          >
+            <g
+              fill="#efefef"
+              fillRule="nonzero"
+              stroke="none"
+              strokeWidth="1"
+              strokeLinecap="butt"
+              strokeLinejoin="miter"
+              strokeMiterlimit="10"
+              strokeDasharray=""
+              strokeDashoffset="0"
+              fontFamily="none"
+              fontWeight="none"
+              fontSize="none"
+              textAnchor="none"
+              style={{ mixBlendMode: "normal" }}
+            >
+              <g transform="scale(10.66667,10.66667)">
+                <path d="M12.545,12.151v0c0,1.054 0.855,1.909 1.909,1.909h3.536c-0.607,1.972 -2.101,3.467 -4.26,3.866c-3.431,0.635 -6.862,-1.865 -7.19,-5.339c-0.34,-3.595 2.479,-6.62 6.005,-6.62c1.002,0 1.946,0.246 2.777,0.679c0.757,0.395 1.683,0.236 2.286,-0.368v0c0.954,-0.954 0.701,-2.563 -0.498,-3.179c-1.678,-0.862 -3.631,-1.264 -5.692,-1.038c-4.583,0.502 -8.31,4.226 -8.812,8.809c-0.661,6.03 4.043,11.13 9.939,11.13c6.368,0 8.972,-4.515 9.499,-8.398c0.242,-1.78 -1.182,-3.352 -2.978,-3.354l-4.61,-0.006c-1.055,-0.002 -1.911,0.853 -1.911,1.909z"></path>
+              </g>
+            </g>
+          </svg>
+          <span className="ml-4">Sign in with Google</span>
         </div>
-
+        <div className="divider px-20"></div>
+        <div className="text-center mt-4 px-2">
+          Don't have an account?
+          <div className="text-purple-700 link focus:translate-y-1 hover:text-purple-900 transition duration-100">
+            Register
+          </div>
+        </div>
       </div>
     </>
   );
