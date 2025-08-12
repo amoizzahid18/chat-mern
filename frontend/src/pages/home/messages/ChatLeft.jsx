9f6ec2c8a7e262d15d912dd1a34b38660db82295
Moiz <a.moiz.zahid.18@gmail.com>
2025-08-13 00:00:39 +0500

    Add message options

--- frontend/src/pages/home/messages/ChatLeft.jsx
+++ frontend/src/pages/home/messages/ChatLeft.jsx
@@ -1,13 +1,126 @@
 import React from "react";
+import { useState } from "react";
 
 function ChatLeft() {
+  const messageOptions = (
+    <button className="dropdown dropdown-end btn shadow-none z-2  bg-neutral h-4 w-2 border-none ">
+      <div tabIndex={0} role="button" className="">
+        <svg
+          className="h-[1em]  text-gray-50 "
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-4"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </g>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content translate-y-14 menu bg-base-300 rounded-box z-10 w-36 p-2 shadow-sm"
+      >
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7.49 12 3.74 8.248m0 0 3.75-3.75m-3.75 3.75h16.5V19.5"
+              />
+            </svg>
+            <a>Reply</a>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+              />
+            </svg>
+            <a>Copy</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m16.49 12 3.75-3.751m0 0-3.75-3.75m3.75 3.75H3.74V19.5"
+              />
+            </svg>
+
+            <a>Forward</a>
+          </div>
+        </li>
+      </ul>
+    </button>
+  );
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="chat chat-start mx-6">
-      <div className="chat-bubble chat-bubble-neutral text-wrap max-w-2/3">
-        <span>Put me on the Council and not make me a Master!??</span>
-        <span className="w-12 flex flex-col justify-end">
-          <time className="text-xs opacity-50">2 hour ago</time>
-        </span>
+    <div className="px-6">
+      <div className="chat chat-start ">
+        <div
+          className="chat-bubble chat-bubble-neutral py-0 pr-0 text-wrap "
+          onMouseEnter={() => setShowModal(true)}
+          onMouseLeave={() => setShowModal(false)}
+        >
+          <div
+            className={`w-full flex justify-end  ${
+              showModal ? "" : "opacity-0"
+            }`}
+          >
+            {messageOptions}
+          </div>
+
+          <span className="mr-3">
+            Put me on the Council and not make me a Master!??
+          </span>
+          <span className="flex justify-end mr-3">
+            <time className="text-xs opacity-50">00:00 am</time>
+          </span>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/home/messages/ChatRight.jsx
+++ frontend/src/pages/home/messages/ChatRight.jsx
@@ -1,13 +1,162 @@
 import React from "react";
+import { useState } from "react";
 
 function ChatRight() {
+  const messageOptions = (
+    <button className="dropdown dropdown-end btn z-2 shadow-none bg-base-300 h-4 w-2 border-none ">
+      <div tabIndex={0} role="button" className="">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-4"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.0"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+            />
+          </g>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content translate-y-14 menu bg-base-300 rounded-box z-10 w-36 p-2 shadow-sm"
+      >
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7.49 12 3.74 8.248m0 0 3.75-3.75m-3.75 3.75h16.5V19.5"
+              />
+            </svg>
+            <a>Reply</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+              />
+            </svg>
+            <a>Edit</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+              />
+            </svg>
+            <a>Copy</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m16.49 12 3.75-3.751m0 0-3.75-3.75m3.75 3.75H3.74V19.5"
+              />
+            </svg>
+
+            <a>Forward</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2.0"
+              stroke="currentColor"
+              class="size-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+
+            <a>Delete</a>
+          </div>
+        </li>
+      </ul>
+    </button>
+  );
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div>
-      <div className="chat chat-end mx-6">
-        <div className="chat-bubble text-wrap max-w-2/3">
-          <span>Put me on the Council and not make me a Master!??</span>
-          <span className="w-12 flex flex-col justify-end">
-            <time className="text-xs opacity-50">2 hour ago</time>
+    <div className="px-6">
+      <div className="chat chat-end ">
+        <div
+          className="chat-bubble py-0 pr-0 text-wrap "
+          onMouseEnter={() => setShowModal(true)}
+          onMouseLeave={() => setShowModal(false)}
+        >
+          <div
+            className={`w-full flex justify-end  ${
+              showModal ? "" : "opacity-0"
+            }`}
+          >
+            {messageOptions}
+          </div>
+
+          <span className="mr-3">
+            Put me on the Council and not make me a Master!??
+          </span>
+          <span className="flex justify-end mr-3">
+            <time className="text-xs opacity-50">00:00 am</time>
           </span>
         </div>
       </div>
