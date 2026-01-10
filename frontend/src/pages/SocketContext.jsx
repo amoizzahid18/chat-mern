dc73e95e670b0e76f5ec57bf3e1364bdb95c1e36
Moiz <a.moiz.zahid.18@gmail.com>
2026-01-10 18:17:57 +0500

    Change sock env url

--- frontend/src/pages/SocketContext.jsx
+++ frontend/src/pages/SocketContext.jsx
@@ -11,7 +11,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     // create socket ONCE for app lifetime
-    socketRef.current = io(import.meta.env.VITE_API_URL, {
+    socketRef.current = io(`${import.meta.env.VITE_API_URL}`, {
       withCredentials: true,
       reconnection: true,
       autoConnect: true,
