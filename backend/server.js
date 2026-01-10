b900de796eb1ea2234703f7ac1f99ca90750b554
Moiz <a.moiz.zahid.18@gmail.com>
2026-01-10 17:27:51 +0500

    Change cors to new vercel dom

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "https://chat-mern-lake.vercel.app", // your frontend URL
     credentials: true,
   })
 );
